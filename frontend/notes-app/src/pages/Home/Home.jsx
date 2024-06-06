9011608f62a99fdcb41d5e0b982c427e751a12d1
adeeburrahman11 <adeeburrahman9021@gmail.com>
2024-06-06 13:27:52 +0530

    note card added static

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
+import NoteCard from "../../components/Cards/NoteCard";
 
 const Home = () => {
   return (
     <>
       <NavBar />
+
+      <div className="container mx-auto">
+        <NoteCard
+          title="Meeting on 7th"
+          date="3rd Aor 2024"
+          content="bskdfg  sbdfy gfiyas usfiskhfk gfyasdfkh fsfbks sfh gf hgfia hgnkjhg hoergj eou hgiueht kouewrhtieoir oengoierhg rgouerg eroihge80rgoer"
+          tags="#Meeting"
+          isPinned={true}
+          onEdit={() => {}}
+          onDelete={() => {}}
+          onPinNote={() => {}}
+        />
+      </div>
     </>
   );
 };
