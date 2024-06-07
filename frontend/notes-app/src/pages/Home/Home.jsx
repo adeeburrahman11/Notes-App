2ae443383e266a3d6c6a8bc3c55f12eb34a12555
adeeburrahman11 <adeeburrahman9021@gmail.com>
2024-06-07 13:59:45 +0530

    .

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -8,16 +8,18 @@ const Home = () => {
       <NavBar />
 
       <div className="container mx-auto">
-        <NoteCard
-          title="Meeting on 7th"
-          date="3rd Aor 2024"
-          content="bskdfg  sbdfy gfiyas usfiskhfk gfyasdfkh fsfbks sfh gf hgfia hgnkjhg hoergj eou hgiueht kouewrhtieoir oengoierhg rgouerg eroihge80rgoer"
-          tags="#Meeting"
-          isPinned={true}
-          onEdit={() => {}}
-          onDelete={() => {}}
-          onPinNote={() => {}}
-        />
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title="Meeting on 7th"
+            date="3rd Aor 2024"
+            content="bskdfg  sbdfy gfiyas usfiskhfk gfyasdfkh fsfbks sfh gf hgfia hgnkjhg hoergj eou hgiueht kouewrhtieoir oengoierhg rgouerg eroihge80rgoer"
+            tags="#Meeting"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
       </div>
     </>
   );
