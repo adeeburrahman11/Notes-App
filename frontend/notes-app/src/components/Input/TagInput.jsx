5c762d4b996623690bac0952cea840ad6bd14165
adeeburrahman11 <adeeburrahman9021@gmail.com>
2024-06-08 14:41:47 +0530

    .

--- frontend/notes-app/src/components/Input/TagInput.jsx
+++ frontend/notes-app/src/components/Input/TagInput.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { MdAdd, MdClose } from "react-icons/md";
 
-const TagInput = () => {
-  return <div>TagInput</div>;
+const TagInput = ({ tags, setTags }) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <div className="flex items-center gap-4 mt-3">
+        <input
+          type="text"
+          className="text-sm bg-transparent border px-3 py-2 rounded outline-none"
+          placeholder="Add tags"
+          onChange={handleInputChange}
+        />
+
+        <button className="w-8 h-8 flex items-center justify-center rounded border border-blue-700 hover:bg-blue-700">
+          <MdAdd className="text-2xl text-blue-700 hover:text-white" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default TagInput;
