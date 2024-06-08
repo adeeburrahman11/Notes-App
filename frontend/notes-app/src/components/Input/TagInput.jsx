1ab50f2bbb42829e30680939183129b5fe961139
adeeburrahman11 <adeeburrahman9021@gmail.com>
2024-06-08 23:57:17 +0530

    prtially completed tag input

--- frontend/notes-app/src/components/Input/TagInput.jsx
+++ frontend/notes-app/src/components/Input/TagInput.jsx
@@ -27,10 +27,13 @@ const TagInput = ({ tags, setTags }) => {
 
   return (
     <div>
-      {tags.length > 0 && (
+      {tags?.length > 0 && (
         <div className="flex items-center gap-2 flex-wrap mt-2">
           {tags.map((tag, index) => {
-            <span key={index} className="">
+            <span
+              key={index}
+              className="flex items-center gap-2 text-sm text-slate-900 bg-slate-100 px-3 py-1 rounded"
+            >
               #{tag}
               <button
                 onClick={() => {
@@ -46,6 +49,7 @@ const TagInput = ({ tags, setTags }) => {
       <div className="flex items-center gap-4 mt-3">
         <input
           type="text"
+          value={inputValue}
           className="text-sm bg-transparent border px-3 py-2 rounded outline-none"
           placeholder="Add tags"
           onChange={handleInputChange}
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -44,9 +44,13 @@ const Home = () => {
         onRequestClose={() => {}}
         style={{ overlay: { backgroundColor: "rgba(0,0,0,0.2)" } }}
         contentLabel=""
-        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow"
       >
-        <AddEditNotes />
+        <AddEditNotes
+          onClose={() => {
+            setOpenAddEditmodal({ isShown: false, type: "add", data: null });
+          }}
+        />
       </Modal>
     </>
   );
