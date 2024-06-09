3026ea981b98b71ba0b569bbb35b7b6b712f6138
adeeburrahman11 <adeeburrahman9021@gmail.com>
2024-06-09 22:52:37 +0530

    .

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -47,6 +47,8 @@ const Home = () => {
         className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow"
       >
         <AddEditNotes
+          type={openAddEditModal.type}
+          noteData={openAddEditModal.data}
           onClose={() => {
             setOpenAddEditmodal({ isShown: false, type: "add", data: null });
           }}
