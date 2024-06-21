364565f2a07283b86438176573e3e478c5f0c660
adeeburrahman11 <adeeburrahman9021@gmail.com>
2024-06-21 23:42:14 +0530

    empy notes

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -7,6 +7,8 @@ import Modal from "react-modal";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 import Toast from "../../components/ToastMessage/Toast";
+import EmptyCard from "../../components/EmptyCard/EmptyCard";
+import AddNotesImg from "../../assets/images/add-notes.svg";
 
 const Home = () => {
   const [openAddEditModal, setOpenAddEditmodal] = useState({
@@ -103,23 +105,29 @@ const Home = () => {
   return (
     <>
       <NavBar userInfo={userInfo} />
-
       <div className="container mx-auto">
-        <div className="grid grid-cols-3 gap-4 mt-8">
-          {allNotes.map((item, index) => (
-            <NoteCard
-              key={item._id}
-              title={item.title}
-              date={item.createdOn}
-              content={item.content}
-              tags={item.tags}
-              isPinned={item.isPinned}
-              onEdit={() => handleEdit(item)}
-              onDelete={() => deleteNote(item)}
-              onPinNote={() => {}}
-            />
-          ))}
-        </div>
+        {allNotes.length > 0 ? (
+          <div className="grid grid-cols-3 gap-4 mt-8">
+            {allNotes.map((item, index) => (
+              <NoteCard
+                key={item._id}
+                title={item.title}
+                date={item.createdOn}
+                content={item.content}
+                tags={item.tags}
+                isPinned={item.isPinned}
+                onEdit={() => handleEdit(item)}
+                onDelete={() => deleteNote(item)}
+                onPinNote={() => {}}
+              />
+            ))}
+          </div>
+        ) : (
+          <EmptyCard
+            imgSrc={AddNotesImg}
+            message={`Welcome to your personal notebook! Your ideas, dreams, and plans are waiting to be captured. Start by clicking the 'Add' button for adding your first note and let your creativity flow.`}
+          />
+        )}
       </div>
       <button
         className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
@@ -129,7 +137,6 @@ const Home = () => {
       >
         <MdAdd className="text-[32px] text-white" />
       </button>
-
       <Modal
         isOpen={openAddEditModal.isShown}
         onRequestClose={() => {}}
