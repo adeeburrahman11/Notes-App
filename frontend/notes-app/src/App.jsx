898421480d141188a2f195355831bd34c5e236e7
adeeburrahman11 <adeeburrahman9021@gmail.com>
2024-06-23 13:06:58 +0530

    .

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from "./pages/SignUp/SignUp";
 const routes = (
   <Router>
     <Routes>
+      <Route path="/" exact element={<LogIn />} />
       <Route path="/dashboard" exact element={<Home />} />
       <Route path="/login" exact element={<LogIn />} />
       <Route path="/signup" exact element={<SignUp />} />
